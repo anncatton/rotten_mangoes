16a9598c83201da78ff348a9b0fbf77e9a8f5cba
Ann Catton <anncatton@gmail.com>
2015-11-16 20:22:19 +0000

    displays form errors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+
+    validates :title, :director, :description, :poster_image_url, :release_date, presence: true
+    validates :runtime_in_minutes,
+        numericality: { only_integer: true }
+
+    validate :release_date_is_in_the_past
+
+    def release_date_is_in_the_past
+      if release_date.present?
+        errors.add(:release_date, "should be in the past") if release_date > Date.today
+      end
+    end
+
 end
