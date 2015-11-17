494950773a463d7c75ad830a3874aaaf64c9bed4
Ann Catton <anncatton@gmail.com>
2015-11-17 21:58:33 +0000

    users can sign up as admins, privileges based on admin status

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,18 @@
 class Admin::UsersController < ApplicationController
-  
+
+  before_filter :require_admin
+
+  def require_admin
+    unless current_user.admin?
+      session[:flash] = "Sorry, not for you!"
+      @flash = session[:flash]
+      redirect_to '/'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+
 end
