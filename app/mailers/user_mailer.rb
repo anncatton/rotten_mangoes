1985acaef3dd923324e0f2b18227840f04333625
Ann Catton <anncatton@gmail.com>
2015-11-18 21:41:50 +0000

    added UserMailer with a delete email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ActionMailer::Base
+
+  default from: 'notifications@rotten_mangoes.com'
+  layout 'mailer'
+ 
+  def delete_email(user)
+    @user = user
+    @url  = 'http://rotten_mangoes.com/login'
+    mail(to: @user.email, subject: "Sorry, we had to destroy you")
+  end
+
+end
